d4cbbd4ec52d4c5d90a4e4057ae2f79826e3324b
Vitaliy <vk.vital@gmail.com>
2020-01-14 20:33:00 +0200

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -31,5 +31,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_not flash.empty?
     assert flash[:success] == 'Welcome to the Sample App!'
+    assert is_logged_in?
   end
 end
