d9babc492250bc3c782b7c3062e8ec91187fb7c3
Vitaliy <vk.vital@gmail.com>
2020-01-19 00:21:49 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
-     else
+      redirect_back_or @user 
+    else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
